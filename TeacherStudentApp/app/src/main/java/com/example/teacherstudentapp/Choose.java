e2dd50062ade3176113aa509c5d9dcef3732c48e
as4nd <as4nd@mail.umsl.edu>
2019-11-02 12:32:59 -0500

    Copied Base File

---
+++ TeacherStudentApp/app/src/main/java/com/example/teacherstudentapp/Choose.java
@@ -0,0 +1,30 @@
+package com.example.teacherstudentapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+public class Choose extends AppCompatActivity {
+
+    private TextView iBuddytextView, textViewChoose;
+    private Button mradioButtonOrganizer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+    }
+
+    miBuddytextView= (TextView) find
+
+
+//    mRegister= (Button) findViewById(R.id.registration);
+  //  mEmail= (EditText) findViewById(R.id.email);
+
+}
---
+++ TeacherStudentApp/app/src/main/java/com/example/teacherstudentapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.teacherstudentapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
